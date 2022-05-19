a51e11e3a38573f7a0e4d9918ea2bb800d64b404
oz <osazeowie0@gmail.com>
2022-05-19 01:03:22 +0100

    feat: replace quoteTxt and author arrays with an API

--- script.js
+++ script.js
@@ -1,57 +1,38 @@
-//My first javaScript project with no tutorials
-//A Random Quote Generator
-
 //Assigning variables to HTML elements
 const quoteTxt = document.getElementById("quotes");
-const aurthor = document.getElementById("aurthor");
+const author = document.getElementById("aurthor");
 const quoteBtn = document.getElementById("btn");
 const bgColor = document.getElementById("body");
 const borderColor = document.getElementById("container");
 
 /*Function that randomly displays a quote with the corresponding
-aurthors name and also changes the color of the background
+aurthors name with the use of an API and also changes the color of the background
 */
-const changeText = () => {
+function generateQuote() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
+            'X-RapidAPI-Key': '5cc4be791bmsh4f1e14656a36b6dp1bd2e9jsnba59f6029352'
+        }
+    };
+    
+    fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+        .then(response => response.json())
+        .then(response => {
+            quoteTxt.innerHTML = response.content;
+            author.innerHTML = response.originator.name;
+        })
+        .catch(err => console.error(err));
 
-    const random = Math.floor(Math.random() * quotesArr.length);
-    quoteTxt.innerHTML = quotesArr[random]
-    aurthor.innerHTML = nameArr[random]
-    bgColor.style.background = bgColorArr[random]
-    borderColor.style.borderColor = borderColorArr[random]
-    quoteBtn.style.backgroundColor = borderColorArr[random]
-    quoteBtn.style.color = "black"
+    const random = Math.floor(Math.random() * bgColorArr.length);
+    bgColor.style.background = bgColorArr[random];
+    borderColor.style.borderColor = borderColorArr[random];
+    quoteBtn.style.backgroundColor = borderColorArr[random];
+    quoteBtn.style.color = "black";
 }
 
-//An array of quotes
-const quotesArr = ["\“A dream is what makes people love life even when it is painful\”",
 
-    "\“True, we love life, not because we are used to living, but because we are used to loving. There is always some madness in love, but there is also always some reason in madness.\”",
-
-    "\“Don’t quit. Suffer now and live the rest of your life as a champion.\”",
-
-    "\ “Don’t grieve. Anything you lose comes around in another form.\”",
-
-    "\“Though nobody can go back and make a new beginning… Anyone can start over and make a new ending.\”",
-
-    "\“Instead of worrying about what you cannot control, shift your energy to what you can create.\”",
-
-    "\“Life is about accepting the challenges along the way, choosing to keep moving forward, and savoring the journey.\”",
-
-    "\“Don’t quit. Suffer now and live the rest of your life as a champion.\”",
-
-    "\“Some beautiful paths can’t be discovered without getting lost.\”",
-
-    "\“The more difficult the victory, the greater the happiness in winning.\”",
-
-    "\ “Sometimes the greatest thing to come out of all your hard work isn’t what you get for it, but what you become for it.\”",
-
-    "\“I am not someone who is ashamed of my past. I’m actually really proud. I know I made a lot of mistakes, but they, in turn, were my life lessons.\”"];
-
-//An array of the aurthors
-const nameArr = ["—Theodore Zeldin", "—Petrach",
-    "—Muhammad Ali", "—Chico Xavier", "—Roy T. Bennett", "—Rumi",
-    "—Roy T. Bennett", "—Muhammad Ali", "—Erol Ozan", "—Pele",
-    "—Steve Maraboli", "—Drew Barrymore"];
 
 //An array of backgroundcolors
 const bgColorArr = ["rgb(46, 69, 96)", "rgb(206, 69, 236)", "rgb(184, 69, 203)",
@@ -64,7 +45,3 @@ const borderColorArr = ["rgb(466, 100, 853)", "rgb(606, 285, 236)", "rgb(184, 28
 "rgb(685, 184, 226)", "rgb(164, 685, 165)", "rgb(185, 612, 141)", 
 "rgb(219, 856, 182)", "rgb(163, 185, 689)", "rgba(163, 685, 978, 0.69)"]
 
-
-
-//The click event that will initiate the changeText function above
-quoteBtn.addEventListener("click", changeText)
